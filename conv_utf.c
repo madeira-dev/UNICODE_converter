28d8c014d0b6cdd9c228fc00c773fb9686639a03
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2022-09-11 15:53:54 -0300

    arquivos base para o inicio do trabalho

---
+++ conv_utf.c
@@ -0,0 +1,13 @@
+/* Gabriel de Oliveira Rosa Mariano Madeira 2111471 3WA */
+/* Juliana Pinheiro dos Santos 2110??? 3WA */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "converteutf.h"
+
+int converteUtf8Para32(FILE *arquivo_entrada, FILE *arquivo_saida)
+{
+}
+int converteUtf32Para8(FILE *arquivo_entrada, FILE *arquivo_saida)
+{
+}
---
+++ converteutf.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+int converteUtf8Para32(FILE *arquivo_entrada, FILE *arquivo_saida);
+int converteUtf32Para8(FILE *arquivo_entrada, FILE *arquivo_saida);
