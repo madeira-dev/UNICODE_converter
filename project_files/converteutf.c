d3adf256a69fdf5dc78938a8227f1bc9eeeb1b2b
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2022-09-12 18:09:28 -0300

    ainda tentando ler o inicio ou BOM o arquivo ainda n entendi a codificacao exata de utf8

--- project_files/converteutf.c
+++ project_files/converteutf.c
@@ -3,30 +3,60 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "converteutf.h"
+
+#define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
+
+// ler o arquivo identificando o numero de bytes que o simbolo ocupa lendo o primeiro byte dele
+// uma vez identificado, fazer switch(?) pra declarar variavel com o tamanho de bytes correspondente
 
 int converteUtf8Para32(FILE *arquivo_entrada, FILE *arquivo_saida)
 {
-    // FILE *entry_file;
-    unsigned int buffer[10];
+    char test;
+    int bin_res;
+    char result;
+    int bom_num;
 
     arquivo_entrada = fopen("utf8_testFile", "rb");
     if (!arquivo_entrada)
     {
-        fprintf(stderr, "erro para abrir arquivo para leitura\n");
+        fprintf(stderr, "erro ao abrir arquivo para leitura\n");
         return -1;
     }
 
-    fread(buffer, sizeof(unsigned int), 1, arquivo_entrada);
+    fread(&bom_num, sizeof(int), 1, arquivo_entrada);
 
-    // printf("%02x", buffer[0]);
+    // printf("%b\n", test); // por que o resultado dele ta dando 1000011????????????????? ta lendo como byte de continuacao??????
+    // printf("%b\n", 0b11110);
 
-    for (int i = 0; i < 2; i++)
-    {
-        printf("%02x ", buffer[i]);
-    }
+    // result = test & 0b11110;
+    // printf("%b\n", result);
 
-    puts("");
+    printf("%b\n", bom_num);
+
+    // switch (result)
+    // {
+    // case 0b0:
+    //     printf("case 0\n");
+    //     break;
+
+    // case 0b10:
+    //     printf("case 10\n"); // byte de continuação
+    //     break;
+
+    // case 0b110:
+    //     printf("case 110\n");
+    //     break;
+
+    // case 0b1110:
+    //     printf("case 1110\n");
+    //     break;
+
+    // case 0b11110:
+    //     printf("case 11110\n");
+    //     break;
+    // }
 
     return 0;
 }
@@ -35,3 +65,4 @@ int converteUtf8Para32(FILE *arquivo_entrada, FILE *arquivo_saida)
 // }
 
 /* qual a diferenca entre esse arquivo e o conv_utf.c??? */
+/* se eu modificar o arquivo acaba a codificação???????? */
