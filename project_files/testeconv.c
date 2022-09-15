b1adfdf1198aa3b2dc17cee1eb671712b1918576
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2022-09-15 18:19:29 -0300

    leitura de utf8 feita, iniciando leitura de utf32

--- project_files/testeconv.c
+++ project_files/testeconv.c
@@ -5,14 +5,20 @@
 int main(void)
 {
 
-    FILE *entry_file;
-    FILE *output_file;
+    // FILE *entry_fileUtf8, *output_fileUtf8;
+    FILE *entry_fileUtf32, *output_fileUtf32;
 
-    entry_file = fopen("utf8_testFileIn", "rb");
-    output_file = fopen("utf8_testFileOut", "wb");
-    converteUtf8Para32(entry_file, output_file);
-    fclose(entry_file);
-    fclose(output_file);
+    // entry_fileUtf8 = fopen("utf8_testFileIn", "rb");
+    // output_fileUtf8 = fopen("utf8_testFileOut", "wb");
+    // converteUtf8Para32(entry_fileUtf8, output_fileUtf8);
+    // fclose(entry_fileUtf8);
+    // fclose(output_fileUtf8);
+
+    entry_fileUtf32 = fopen("utf32_testFileIn", "rb");
+    output_fileUtf32 = fopen("utf32_testFileOut", "wb");
+    converteUtf32Para8(entry_fileUtf32, output_fileUtf32);
+    fclose(entry_fileUtf32);
+    fclose(output_fileUtf32);
 
     return 0;
 }
