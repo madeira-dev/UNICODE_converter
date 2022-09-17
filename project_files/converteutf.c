f2200f86ae8adc22836b83d215c35e59599ddd42
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2022-09-17 16:56:41 -0300

    utf8->utf32: leitura de arquivo e extensão de arquivos pronta; utf32->utf8: caso arquivo lido seja little endian, a conversão já est;a pronta;

--- project_files/converteutf.c
+++ project_files/converteutf.c
@@ -6,10 +6,7 @@
 #include <string.h>
 #include "converteutf.h"
 
-/* ok na vdd a leitura do utf8 ta errada, foi testada apenas a leitura de um arquivo com só um caracter.
-nesse caso a leitura funciona, para ler com mais de um caracter precisa considerar que o divisor entre
-todos eles é um espaço em branco ' '. imagino que basta ler o caracter e testar se é um espaço em hex
-é 20 e em binário é 0010 0000, se for um espaço pula ele de algum jeito e lê o próximo caracter */
+int count_bits(unsigned char byte);
 
 int converteUtf8Para32(FILE *arquivo_entrada, FILE *arquivo_saida)
 {
@@ -17,64 +14,193 @@ int converteUtf8Para32(FILE *arquivo_entrada, FILE *arquivo_saida)
     unsigned char final_value;
     int n_bytes = 0;
 
+    unsigned int BOM_to_write = 0x0000FEFF; /* BOM pra indiciar que o arquivo foi gravado em little endian */
+    fwrite(&BOM_to_write, sizeof(unsigned int), 1, arquivo_saida);
+
     if (!arquivo_entrada)
     {
         fprintf(stderr, "erro ao abrir arquivo para leitura\n");
         return -1;
     }
 
-    /* lendo o primeiro byte do arquivo para saber quantos bytes o caracter ocupa */
-    fread(&first_byte, sizeof(unsigned char), 1, arquivo_entrada);
-    final_value = first_byte;
-
-    /* verificando se o caracter é um espaço para pular para o próximo */
-    if (first_byte == 0x20)
+    while (!feof(arquivo_entrada)) /* ALTERAR CONDICAO DO WHILE. DESSE JEITO ELE LE O ÚLTIMO BYTE DUAS VEZES */
     {
-        unsigned char jmp_space;
-        fread(&jmp_space, sizeof(unsigned char), 1, arquivo_entrada);
-        final_value = jmp_space;
-    }
+        /* lendo o primeiro byte do arquivo para saber quantos bytes o caracter ocupa */
+        fread(&first_byte, sizeof(unsigned char), 1, arquivo_entrada);
 
-    /* loop para descobrir quantos bits 1 no início o primeiro byte apresenta */
-    while ((final_value & 0b10000000) != 0)
-    {
-        final_value = final_value << 1;
-        n_bytes++;
-    }
+        final_value = first_byte;
 
-    if (n_bytes == 0)
-        printf("tmp first_byte n_bytes=0\n");
+        /* loop para descobrir quantos bits 1 no início o primeiro byte apresenta */
+        while ((first_byte & 0b10000000) != 0)
+        {
+            first_byte = first_byte << 1;
+            n_bytes++;
+        }
 
-    else if (n_bytes == 2)
-        printf("tmp first_byte n_bytes=2\n");
+        /* ifs para tratar cada caso de número de bytes e gravar no arquivo novo utf32 */
+        if (n_bytes == 0)
+        {
+            printf("1 byte\n");
 
-    else if (n_bytes == 3)
-    {
-        unsigned char byte1, byte2, byte3;
+            unsigned char byte1;
+            unsigned int mask_to_concat = 0x0, concat_bytes = 0x0;
 
-        /* definindo o primeiro byte como os bits do byte lido exceto os iniciais representando o numero de bytes total */
-        byte1 = final_value & 0b00011111;
+            byte1 = final_value & 0b01111111;
+            printf("n_bytes=0 - byte1: %x\n\n", byte1);
 
-        fread(&byte2, sizeof(char), 1, arquivo_entrada);
-        fread(&byte3, sizeof(char), 1, arquivo_entrada);
+            concat_bytes = byte1;
+            concat_bytes |= mask_to_concat;
+        }
 
-        byte2 = byte2 & 0b00111111; /* & operation para remover bytes de continuacao */
-        byte3 = byte3 & 0b00111111; /* & operation para remover bytes de continuacao */
-    }
+        else if (n_bytes == 2)
+        {
+            printf("2 bytes\n");
 
-    else if (n_bytes == 4)
-    {
-        // unsigned char byte1, byte2, byte3, byte4;
-        printf("tmp first_byte n_bytes=4\n");
+            unsigned char byte1, byte2;
+            unsigned int mask_to_concat = 0x0, concat_bytes = 0x0, total_bits = 0;
+            // unsigned char endianness_byte1; /* primeiro byte a ser gravado (ficar na extrema direita) */
+            // unsigned char endianness_byte2; /* segundo byte a ser gravado (ficar na extrema esquerda) */
+
+            printf("hex: %x\n", final_value);
+
+            fread(&byte2, sizeof(unsigned char), 1, arquivo_entrada);
+
+            byte1 = final_value & 0b00011111; /* definindo o primeiro byte como os bits do byte lido exceto os iniciais representando o numero de bytes total */
+            byte2 = byte2 & 0b00111111;       /* & operation para remover bytes de continuacao */
+
+            printf("n_bytes=2 - byte1: %x\n", byte1);
+            printf("n_bytes=2 - byte2: %x\n\n", byte2);
+
+            concat_bytes = byte1;
+            concat_bytes <<= count_bits(byte2);
+            total_bits += count_bits(byte2);
+            concat_bytes |= byte2;
+            concat_bytes |= mask_to_concat;
+
+            printf("concat_bytes antes de inverter os bytes: %x\n", concat_bytes);
+
+            /* invertendo os bytes para gravar em little endian // ta dando erradozzzzzzzzzzzzzzz*/
+
+            // endianness_byte1 = concat_bytes & 0b1111111100000000; /* talvez tenha q dar bitshift pra direita de 1 byte para fazer o or */
+            // endianness_byte1 = endianness_byte1 >> 8;
+            // endianness_byte2 = concat_bytes & 0b0000000011111111;
+
+            // concat_bytes = 0x0;
+
+            // concat_bytes |= endianness_byte2;
+            // concat_bytes <<= 8;
+            // concat_bytes |= endianness_byte1;
+
+            // printf("concat_bytes depois de inverter os bytes: %x\n", concat_bytes);
+        }
+
+        else if (n_bytes == 3)
+        {
+            printf("3 bytes\n");
+
+            unsigned char byte1, byte2, byte3;
+            unsigned int concat_bytes = 0x0, total_bits = 0, mask_to_concat = 0x0;
+            unsigned char endianness_byte1, endianness_byte2;
+
+            // printf("hex: %x\n", final_value);
+            // printf("bin: %b\n", final_value);
+
+            fread(&byte2, sizeof(unsigned char), 1, arquivo_entrada);
+            fread(&byte3, sizeof(unsigned char), 1, arquivo_entrada);
+
+            byte1 = final_value & 0b00011111; /* definindo o primeiro byte como os bits do byte lido exceto os iniciais representando o numero de bytes total */
+            byte2 = byte2 & 0b00111111;       /* & operation para remover bytes de continuacao */
+            byte3 = byte3 & 0b00111111;       /* & operation para remover bytes de continuacao */
+
+            printf("n_bytes=3 - byte1: %x\n", byte1);
+            printf("n_bytes=3 - byte2: %x\n", byte2);
+            printf("n_bytes=3 - byte3: %x\n\n", byte3);
+
+            concat_bytes = byte1;
+            concat_bytes <<= count_bits(byte2);
+            total_bits += count_bits(byte2);
+            concat_bytes |= byte2;
+            concat_bytes <<= count_bits(byte3);
+            total_bits += count_bits(byte3);
+            concat_bytes |= byte3;
+            concat_bytes |= mask_to_concat;
+            printf("concat_bytes bin antes de inverter: %b\n", concat_bytes);
+            // printf("concat_bytes bin antes de inverter: %b\n", concat_bytes);
+
+            /* invertendo os bytes para gravar em little endian // ta dando erradozzzzzzzzzzzzzzz*/
+
+            endianness_byte1 = (concat_bytes & 0b1111111100000000) >> 8; /* talvez tenha q dar bitshift pra direita de 1 byte para fazer o or */
+            printf("endianness_byte1: %x\n", endianness_byte1);
+            endianness_byte2 = concat_bytes & 0b0000000011111111;
+            printf("endianness_byte2: %x\n", endianness_byte2);
+
+            concat_bytes = 0x0;
+
+            concat_bytes |= endianness_byte2;
+            concat_bytes <<= 8;
+            concat_bytes |= endianness_byte1;
+
+            printf("concat_bytes depois de inverter os bytes em hex: %x\n", concat_bytes);
+            // printf("concat_bytes depois de inverter os bytes em bin: %b\n", concat_bytes);
+        }
+
+        else if (n_bytes == 4)
+        {
+            printf("4 bytes\n");
+
+            unsigned char byte1, byte2, byte3, byte4;
+            unsigned int concat_bytes = 0x0, total_bits = 0, mask_to_concat = 0x0;
+
+            printf("hex: %x\n", final_value);
+
+            fread(&byte2, sizeof(unsigned char), 1, arquivo_entrada);
+            fread(&byte3, sizeof(unsigned char), 1, arquivo_entrada);
+            fread(&byte4, sizeof(unsigned char), 1, arquivo_entrada);
+
+            byte1 = final_value & 0b00000111; /* definindo o primeiro byte como os bits do byte lido exceto os iniciais representando o numero de bytes total */
+            byte2 = byte2 & 0b00111111;       /* & operation para remover bytes de continuacao */
+            byte3 = byte3 & 0b00111111;       /* & operation para remover bytes de continuacao */
+            byte4 = byte4 & 0b00111111;       /* & operation para remover bytes de continuacao */
+
+            printf("n_bytes=4 - byte1: %x\n", byte1);
+            printf("n_bytes=4 - byte2: %x\n", byte2);
+            printf("n_bytes=4 - byte3: %x\n", byte3);
+            printf("n_bytes=4 - byte4: %x\n\n", byte4);
+
+            concat_bytes = byte1;
+            concat_bytes <<= count_bits(byte2);
+            total_bits += count_bits(byte2);
+            concat_bytes |= byte2;
+            concat_bytes <<= count_bits(byte3);
+            total_bits += count_bits(byte3);
+            concat_bytes |= byte3;
+            concat_bytes <<= count_bits(byte4);
+            total_bits += count_bits(byte4);
+            concat_bytes |= byte4;
+            concat_bytes |= mask_to_concat;
+
+            printf("concat_bytes: %x\n", concat_bytes);
+        }
     }
 
     return 0;
 }
 
+int count_bits(unsigned char byte)
+{
+    unsigned int count = 0;
+    while (byte)
+    {
+        count++;
+        byte >>= 1;
+    }
+
+    return count;
+}
+
 // int converteUtf32Para8(FILE *arquivo_entrada, FILE *arquivo_saida)
 // {
-//     unsigned int utf32_BOM;
-//     unsigned int utf32_conteudo;
+//     unsigned int utf32_BOM, utf32_conteudo, final_value, endianness, num_bytes = 0;
 
 //     if (!arquivo_entrada)
 //     {
@@ -82,38 +208,119 @@ int converteUtf8Para32(FILE *arquivo_entrada, FILE *arquivo_saida)
 //         return -1;
 //     }
 
-//     fread(&utf32_BOM, sizeof(int), 1, arquivo_entrada);      /* lendo BOM do arquivo */
-//     fread(&utf32_conteudo, sizeof(int), 1, arquivo_entrada); /* lendo valores do arquivo */
+//     fread(&utf32_BOM, sizeof(unsigned int), 1, arquivo_entrada); /* lendo BOM do arquivo */
 
-//     printf("utf32_BOM: %x\n", utf32_BOM);
-//     printf("utf32_conteudo: %x\n", utf32_conteudo);
-
-//     if (utf32_BOM == 0xFEFF) /* verificando se BOM é little endian */
+//     if (utf32_BOM == 0x0000FEFF) /* verificando se BOM é little endian */
 //     {
-
-//         fwrite(&utf32_conteudo, sizeof(unsigned int), 1, arquivo_saida);
-//         printf("little endian\n");
+//         endianness = 1;
 //     }
 
-//     else if (0x0000FEFF) /* verificando se BOM é big endian */
+//     else if (utf32_BOM == 0xFEFF0000) /* verificando se BOM é big endian */
 //     {
-//         unsigned char byte_final, byte_inicial;
-
-//         byte_final = utf32_conteudo & 0b0000000011111111;
-//         byte_inicial = utf32_conteudo & 0b1111111100000000;
-//         printf("byte_final: %x\n", byte_final);
-//         printf("byte_inicial: %x\n", byte_inicial);
-
-//         fwrite(&byte_inicial, sizeof(unsigned char), 1, arquivo_saida);
-//         fwrite(&byte_final, sizeof(unsigned char), 1, arquivo_saida);
-
-//         printf("big endian");
+//         printf("utf32_BOM: %x\n", utf32_BOM);
+//         printf("big endian\n");
+//         endianness = 0;
 //     }
 
 //     else /* caso de BOM inválido */
 //     {
-//         fprintf(stderr, "BOM invalido");
+//         fprintf(stderr, "BOM invalido\n");
 //         return -1;
+//     }
+
+//     while (!feof(arquivo_entrada)) /* alterar esta condicao do while pq esta repetindo o ultimo byte */
+//     {
+
+//         if (endianness == 1) /* caso little endian */
+//         {
+//             num_bytes = 0;
+
+//             fread(&utf32_conteudo, sizeof(unsigned int), 1, arquivo_entrada); /* lendo valores do arquivo */
+//             final_value = utf32_conteudo;
+
+//             /* verificando quantidade de bytes usados para cada o caracter */
+//             while ((utf32_conteudo & 0x000000FF) != 0)
+//             {
+//                 utf32_conteudo = utf32_conteudo >> 8;
+//                 num_bytes++;
+//             }
+
+//             /* tratando gravação de cada caracter dependendo da quantidade de bytes que ocupa */
+//             if (num_bytes == 1)
+//             {
+//                 unsigned char byte_to_write;
+//                 byte_to_write = final_value & 0x000000FF;
+//                 printf("byte_to_write 1: %x\n", byte_to_write);
+//                 fwrite(&byte_to_write, sizeof(unsigned char), 1, arquivo_saida);
+//             }
+
+//             else if (num_bytes == 2)
+//             {
+//                 unsigned char byte1_to_write, byte2_to_write;
+
+//                 byte1_to_write = final_value & 0x000000FF;
+//                 byte2_to_write = (final_value & 0x0000FF00) >> 8;
+//                 printf("byte_to_write 1: %x\n", byte1_to_write);
+//                 printf("byte_to_write 2: %x\n", byte2_to_write);
+//                 fwrite(&byte2_to_write, sizeof(unsigned char), 1, arquivo_saida);
+//                 fwrite(&byte1_to_write, sizeof(unsigned char), 1, arquivo_saida);
+//             }
+
+//             else if (num_bytes == 3)
+//             {
+
+//                 unsigned char byte1_to_write, byte2_to_write, byte3_to_write;
+//                 byte1_to_write = final_value & 0x000000FF;
+//                 byte2_to_write = (final_value & 0x0000FF00) >> 8;
+//                 byte3_to_write = (final_value & 0x00FF0000) >> 16;
+
+//                 printf("byte_to_write 1: %x\n", byte1_to_write);
+//                 printf("byte_to_write 2: %x\n", byte2_to_write);
+//                 printf("byte_to_write 3: %x\n", byte3_to_write);
+
+//                 fwrite(&byte3_to_write, sizeof(unsigned char), 1, arquivo_saida);
+//                 fwrite(&byte2_to_write, sizeof(unsigned char), 1, arquivo_saida);
+//                 fwrite(&byte1_to_write, sizeof(unsigned char), 1, arquivo_saida);
+//             }
+
+//             else if (num_bytes == 4)
+//             {
+
+//                 unsigned char byte1_to_write, byte2_to_write, byte3_to_write, byte4_to_write;
+//                 byte1_to_write = final_value & 0x000000FF;
+//                 byte2_to_write = (final_value & 0x0000FF00) >> 8;
+//                 byte3_to_write = (final_value & 0x00FF0000) >> 16;
+//                 byte4_to_write = (final_value & 0xFF000000) >> 24;
+
+//                 printf("byte_to_write 1: %x\n", byte1_to_write);
+//                 printf("byte_to_write 2: %x\n", byte2_to_write);
+//                 printf("byte_to_write 3: %x\n", byte3_to_write);
+//                 printf("byte_to_write 4: %x\n", byte4_to_write);
+
+//                 fwrite(&byte4_to_write, sizeof(unsigned char), 1, arquivo_saida);
+//                 fwrite(&byte3_to_write, sizeof(unsigned char), 1, arquivo_saida);
+//                 fwrite(&byte2_to_write, sizeof(unsigned char), 1, arquivo_saida);
+//                 fwrite(&byte1_to_write, sizeof(unsigned char), 1, arquivo_saida);
+//             }
+//         }
+
+//         else /* caso big endian */
+//         {
+//             /* se for big endian tem que inverter e gravar em little endian? */
+//             unsigned char byte_final, byte_inicial;
+
+//             byte_final = final_value & 0x00FF;
+//             byte_inicial = final_value & 0xFF00;
+//             unsigned char complete_value = byte_inicial | byte_final;
+
+//             /* LEMBRAR DE TIRAR ESSES PRINTS */
+//             printf("byte_final: %x\n", byte_final);
+//             printf("byte_inicial: %x\n", byte_inicial);
+//             printf("complete_value: %x\n\n", complete_value);
+
+//             fwrite(&byte_inicial, sizeof(unsigned char), 1, arquivo_saida);
+//             fwrite(&byte_final, sizeof(unsigned char), 1, arquivo_saida);
+//         }
 //     }
 
 //     return 0;
